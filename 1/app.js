e35c058de71d66fd2c89f1f5ac65531b366a34a4
Kuba <jakubik.jb.40@gmail.com>
2022-12-21 21:40:02 +0100

    [1] done

--- 1/app.js
+++ 1/app.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
 function main() {
-    let arr = [], i = 0;
+    let arr = [], i = 0, s = 0;
     fs.readFileSync('./1/data.txt', 'utf-8')
         .split(/\n/)
-        .forEach((l) => l.trim().length ? arr[i]=parseInt(l)+(arr[i] ? parseInt(arr[i]) : 0) : i++)
+        .forEach((l) => l.trim().length ? arr[i]=parseInt(l)+(arr[i] ? parseInt(arr[i]) : 0) : i++);
 
-    console.log(Math.max(...arr))
+    [...arr].sort((a,b) => b-a).slice(0,3).map(v => s+=v);
+
+    console.log(`Part 1: ${Math.max(...arr)} \n`);
+    console.log(`Part 2: ${s}`);
 }
 
 main()
