6b132bb58cb35daf99fe915589115739c8d2c658
Kuba <jakubik.jb.40@gmail.com>
2022-12-22 16:32:58 +0100

    [2] done

--- 2/app.js
+++ 2/app.js
@@ -1,26 +1,28 @@
 const fs = require('fs')
-const c = (n,d,w,l,h) => n[1].trim() === h[0] ? d : (n[1].trim() === h[1] ? w : l)
+const c = (n,d,w,l,h) => n.trim() === h[0] ? d : (n.trim() === h[1] ? w : l)
+const u = (l,f,s,t) => l.trim() === "A" ? f : (l.trim() === "B" ? s : t)
 function main() {
-    let p = 0
+    let p1 = 0,
+        p2 = 0
     const d = {'A':'X', 'B':'Y', 'C':'Z'}
 
     fs.readFileSync('2/data.txt', 'utf-8')
         .split(/\n/)
         .forEach(l => {
             l = l.split(" ")
-            switch (d[l[0]].trim()) {
-                case "X":
-                    p+=c(l,4,8,3, ["X", "Y"])
-                break;
-                case "Y":
-                    p+=c(l,5,9,1, ["Y", "Z"])
-                break;
-                case "Z":
-                    p+=c(l,6,7,2, ["Z", "X"])
-                break;
+            switch (d[l[0]]) {
+                case "X": p1+=c(l[1],4,8,3, ["X", "Y"]); break;
+                case "Y": p1+=c(l[1],5,9,1, ["Y", "Z"]); break;
+                case "Z": p1+=c(l[1],6,7,2, ["Z", "X"]); break;
+            }
+            switch (l[1].trim()) {
+                case "X": p2+=u(l[0],3,1,2);   break;
+                case "Y": p2+=u(l[0],1,2,3)+3; break;
+                case "Z": p2+=u(l[0],2,3,1)+6; break;
             }
         })
 
-    console.log(`Part 1: ${p}`)
+    console.log(`Part 1: ${p1}`)
+    console.log(`Part 2: ${p2}`)
 }
 main()
