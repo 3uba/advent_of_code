3012c5974588f89fe5ab0870d7c294e6231b734c
Kuba <jakubik.jb.40@gmail.com>
2022-12-25 21:45:59 +0100

    [4] Part 1

--- 4/app.js
+++ 4/app.js
@@ -1,7 +1,12 @@
 const fs = require('fs')
+let i = 0
 function main() {
-    const text = fs.readFileAsync('4/data.txt', 'utf-8').split(/\n/)
-
-
+    fs.readFileSync('./4/data.txt', 'utf-8')
+        .split(/\n/)
+        .forEach(line => {
+            let [n1,n2,n3,n4] = line.replace(/\r/, '').split(/[-,]+/).map(Number)
+            if ((n1 <= n3 && n2 >= n4) || (n1 >= n3 && n2 <= n4)) i++
+        })
+    console.log(`Part 1: ${i}`)
 }
 main()
